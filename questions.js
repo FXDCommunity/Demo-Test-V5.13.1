4f39a226a84bba65834a33c1a9ef3d144c391363
FXDCommunity <166769846+FXDCommunity@users.noreply.github.com>
2024-05-01 11:50:18 +0800

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,72 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "???",
+    answer: "???",
+    options: [
+      "???",
+      "???",
+      "???",
+      "???"
+    ]
+  },
+    {
+    numb: 2,
+    question: "???",
+    answer: "???",
+    options: [
+      "???",
+      "???",
+      "???",
+      "???"
+    ]
+  },
+    {
+    numb: 3,
+    question: "???",
+    answer: "???",
+    options: [
+      "???",
+      "???",
+      "???",
+      "???"
+    ]
+  },
+    {
+    numb: 4,
+    question: "???",
+    answer: "???",
+    options: [
+      "???",
+      "???",
+      "???",
+      "???"
+    ]
+  },
+    {
+    numb: 5,
+    question: "???",
+    answer: "???",
+    options: [
+      "???",
+      "???",
+      "???",
+      "???"
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
